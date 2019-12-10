e429ea282c5f25cf60704dfb4f675e52aa0cae8d
YurySinev <shytun@gmail.com>
2019-12-10 20:47:34 +0400

    Init commit. Jekyll blog with language switcher and disqus comments loading by pressing a button

---
+++ ru/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"приветствие на русском   YEAR-MONTH-DAY-title.MARKUP   Where YEAR is a four-digit number, MONTH and DAY are both two-digit numbers, and MARKUP is the file extension representing the format used in the file. After that, include the necessary front matter. Take a look at the source for this post to get an idea about how it works.   Jekyll also offers powerful support for code snippets:   def print_hi(name)   puts \"Hi, #{name}\" end print_hi('Tom') #=&gt; prints 'Hi, Tom' to STDOUT.  Check out the Jekyll docs for more info on how to get the most out of Jekyll. File all bugs/feature requests at Jekyll’s GitHub repo. If you have questions, you can ask them on Jekyll Talk.   ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "https://flawed.blog/welcome/",
+        "teaser":"https://flawed.blog/assets/images/sampleCode.png"},{
+        "title": "Test",
+        "excerpt":"test  ","categories": ["test"],
+        "tags": [],
+        "url": "https://flawed.blog/test/",
+        "teaser":"https://flawed.blog/assets/images/sampleCode.png"}]
